570d28e742f64ac220b561b292ea4657e97b6e16
Alexandru Gherghel <alexandru.gherghel@ascentcore.com>
2018-05-02 14:21:12 +0300

    [DEV-20154] Increased max length for state field.

--- django_geo/models.py
+++ django_geo/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 ADDRESS_DISPLAY_CHARS = 25
 
+
 class ZipCode(models.Model):
     zip_code = models.CharField(db_index=True, max_length=10)
     latitude = models.DecimalField(db_index=True, max_digits=10, decimal_places=6)
     longitude = models.DecimalField(db_index=True, max_digits=10, decimal_places=6)
-    state = models.CharField(max_length=2)
+    state = models.CharField(max_length=150)
     city = models.CharField(max_length=30)
+
     def __unicode__(self):
         return self.zip_code
