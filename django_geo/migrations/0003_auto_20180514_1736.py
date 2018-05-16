7969ee6ee2cd35af1ce95521efbfafbdd1c49df5
Eric Plumb <eric.plumb@gmail.com>
2018-05-16 12:40:46 -0700

    [DEV-20631] Chunk update into batches of 1000

--- django_geo/migrations/0003_auto_20180514_1736.py
+++ django_geo/migrations/0003_auto_20180514_1736.py
@@ -4,18 +4,32 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
-ADD_COUNTRY_CODE_SQL = """
-UPDATE django_geo_zipcode
-SET country_code = 
-  CASE WHEN zip_code REGEXP '[0-9]{5}'
-    THEN 'US'
-    ELSE 'CA'
-  END;
-"""
 
-BLANK_COUNTRY_CODE_SQL = """
-UPDATE django_geo_zipcode SET country_code = '';
-"""
+def set_country_codes(apps, schema_editor):
+    # Set country_code on ZipCode objects in batches
+    # (so as not to read/update 1M rows)
+    batch_size = 1000
+    if schema_editor.connection.alias == 'default':
+        ZipCode = apps.get_model('django_geo', 'ZipCode')
+        offset = 0
+        while True:
+            ids = ZipCode.objects.order_by('id').values_list('id', flat=True)[offset:offset+batch_size]
+            if not ids.exists():
+                break
+
+            ZipCode.objects.filter(id__in=list(ids)).update(
+                country_code=models.Case(
+                    models.When(zip_code__regex='[0-9]{5}',
+                                then=models.Value('US')),
+                    default=models.Value('CA')
+                )
+            )
+            offset += batch_size
+
+
+def blank_country_codes(apps, *args):
+    ZipCode = apps.get_model('django_geo', 'ZipCode')
+    ZipCode.objects.all().update(country_code='')
 
 
 class Migration(migrations.Migration):
@@ -30,9 +44,9 @@ class Migration(migrations.Migration):
             name='country_code',
             field=models.CharField(blank=True, default=b'', max_length=2),
         ),
-        migrations.RunSQL(
-            ADD_COUNTRY_CODE_SQL,
-            reverse_sql=BLANK_COUNTRY_CODE_SQL,
+        migrations.RunPython(
+            set_country_codes,
+            blank_country_codes,
         ),
         migrations.AlterUniqueTogether(
             name='zipcode',
